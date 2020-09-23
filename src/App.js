c53e0ea4662202cff4e8f353f48f52d59c6ae27c
javaclick <woodguitar7@gmail.com>
2020-09-23 18:37:35 +0900

    master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2> This is {name} </h2>
+      <img src={picture} />
+    </div>
+  
+  );
+}
+
+const foodILike = [
+  {
+    name: 'Galaxy',
+    image: 'http://img.danawa.com/prod_img/500000/897/239/img/12239897_1.jpg?shrink=160:160&_v=20200915090613',
+  },
+  {
+    name: 's20',
+    image: 'http://img.danawa.com/prod_img/500000/201/359/img/10359201_1.jpg?shrink=160:160&_v=20200417124920',
+  },
+  {
+    name: 'iPhone',
+    image: 'http://img.danawa.com/prod_img/500000/515/571/img/9571515_1.jpg?shrink=160:160&_v=20191119100510',
+  },
+];
+
+
+function renderFood(dish) {
+  return <Food name={dish.name} picture={dish.image} />;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(renderFood)}
     </div>
   );
 }
