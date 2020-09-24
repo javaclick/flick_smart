c284b29fcda48e84bf3417e43a61110a7cdf0375
javaclick <woodguitar7@gmail.com>
2020-09-24 18:43:29 +0900

    master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Food({ name, picture }) {
   return (
     <div>
-      <h2> This is {name} </h2>
-      <img src={picture} />
+      <h2>This is {name} </h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}/>
     </div>
   
   );
@@ -12,28 +14,33 @@ function Food({ name, picture }) {
 
 const foodILike = [
   {
+    id: 1,
     name: 'Galaxy',
     image: 'http://img.danawa.com/prod_img/500000/897/239/img/12239897_1.jpg?shrink=160:160&_v=20200915090613',
+    rating: 5,
   },
   {
+    id: 2,
     name: 's20',
     image: 'http://img.danawa.com/prod_img/500000/201/359/img/10359201_1.jpg?shrink=160:160&_v=20200417124920',
+    rating: 4.5,
   },
   {
+    id: 3,
     name: 'iPhone',
     image: 'http://img.danawa.com/prod_img/500000/515/571/img/9571515_1.jpg?shrink=160:160&_v=20191119100510',
+    rating: 4,
   },
 ];
 
 
-function renderFood(dish) {
-  return <Food name={dish.name} picture={dish.image} />;
-}
 
 function App() {
   return (
     <div>
-      {foodILike.map(renderFood)}
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+      ))}
     </div>
   );
 }
