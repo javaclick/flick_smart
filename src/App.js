081be87c777599188ba5d38ad540a7816c7c4e5c
javaclick <65999751+javaclick@users.noreply.github.com>
2020-10-03 23:26:41 +0900

    master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
+    state = {
+        isLoading: true,
+        movies: [],
+    };
+
+    getMovies = async () => {
+        const {
+            data: {
+                data: {movies},
+            },
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+
+        this.setState({ movies, isLoading: false });  // ES6 {movies:movies} => movies 로 축약 (키와 변수의 이름이 동일한 경우 가능)
+    };
+
+    componentDidMount() {
+        this.getMovies();
+    }
+
     render() {
+        const { isLoading, movies } = this.state;
         return (
             <div>
-                <h3>movie app</h3>
+                {isLoading
+                 ? 'Loading...'
+                 : movies.map((movie) => {
+                     console.log(movie);
+                     return <Movie
+                         key={movie.id}
+                         id={movie.id}
+                         year={movie.year}
+                         title={movie.title}
+                         summary={movie.summary}
+                         poster={movie.medium_cover_image}
+                     / >;
+                 })}
             </div>
         );
     }
